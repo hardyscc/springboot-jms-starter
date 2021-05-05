5d49f5f049a333f76f851906de8a9b4fefc7e92d
Hardys <hardyscc@gmail.com>
2021-05-05 16:05:49 +0800

    first commit

---
+++ src/main/java/com/example/sjs/SpringbootJmsStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.sjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJmsStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJmsStarterApplication.class, args);
+	}
+
+}
